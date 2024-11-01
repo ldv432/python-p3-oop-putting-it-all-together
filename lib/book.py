f4381aa0ea56011507d119146d008bd11ec23c0d
ldv432 <ldv432@gmail.com>
2024-10-31 19:12:36 -0600

    wow we put it all together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
     
+    @page_count.setter 
+    def page_count(self, page_count):
+        if not isinstance(page_count, int): 
+            print("page_count must be an integer")
+        else:
+            self._page_count = page_count
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+
         
+    # page_count = property(get_page_count, set_page_count)
+        
+    
+# book = Book("And Then There Were None", 272)
+# import ipdb; ipdb.set_trace()    
